f0e87f302c2793187cb64c4c7ba8cdb4a179a626
SpunkyCheetah <Iliana.Bernadette@gmail.com>
2023-11-06 13:56:04 -0800

    Player can now move

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement")]
+    public float moveSpeed;
+    public float jumpForce;
+    public bool isOnGround;
+    private Rigidbody2D playerRB;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRB = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            playerRB.AddForce(Vector2.right * moveSpeed, ForceMode2D.Impulse);
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            playerRB.AddForce(Vector2.left * moveSpeed, ForceMode2D.Impulse);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRB.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
     }
 }
